e5ae14fa47202c15f5c8a0947ac2d8ce1aea70dc
telegramxmanbot <mohammadrezam053@gmail.com>
2016-03-28 06:41:07 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626081},--Sudo users
+    sudo_users = {188564034},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
